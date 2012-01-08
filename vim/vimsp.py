e5846df6736a6d98288839e41876ae1fa80f2b37
Fahrzin Hemmati <fahhem@fahhem.com>
2012-01-07 17:31:42 -0800

    Allow all vertical splits in vimsp.py

--- vim/vimsp.py
+++ vim/vimsp.py
@@ -15,11 +15,23 @@ def neighborhood(iterable):
         item = next
     yield (prev,item,None)
 
+HSPLIT,VSPLIT = 'split','vsplit'
+def hsplit(file): return HSPLIT+' '+file
+def vsplit(file): return VSPLIT+' '+file
 
 import sys
 files = sys.argv[1:]
 
 numlines = files.count('/')
+if numlines == 1 and files[0] == '/':
+    # make all hsplits into vsplits
+    oldfiles = files[1:]
+    files = []
+    for file in oldfiles:
+        files.append(file)
+        files.append('/')
+    files = files[:-1] # remove the last /
+    numlines = len(oldfiles)
 
 args = ['vim'] #,'-c','set splitright']
 
@@ -27,7 +39,7 @@ args = ['vim'] #,'-c','set splitright']
 # each split goes up and opens that file there
 for prev, file, next in neighborhood(files):
     if next and next=='/': # last file of a line
-        args.extend(['-c','split %s' % file])
+        args.extend(['-c',hsplit(file)])
 
 # then split left-right for each
 # we're starting out in the top split
@@ -35,7 +47,7 @@ for prev, file, next in neighborhood(files):
     if file=='/': # move down
         args.extend(['-c','wincmd j'])
     elif next and next!='/': # don't touch the last file
-        args.extend(['-c','vsplit %s' % file])
+        args.extend(['-c',vsplit(file)])
 
 # move to the top
 for i in range(numlines):
